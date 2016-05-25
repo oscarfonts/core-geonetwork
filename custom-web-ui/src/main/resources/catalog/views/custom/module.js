da96f7b9f737f8f5c336344fb9d111737c6a9090
Oscar Fonts <oscar.fonts@gmail.com>
2016-05-25 11:54:34 +0200

    Round two

--- custom-web-ui/src/main/resources/catalog/views/custom/module.js
+++ custom-web-ui/src/main/resources/catalog/views/custom/module.js
@@ -2,9 +2,10 @@
 
   goog.provide('gn_search_custom');
 
+  goog.require('gn_search');
   goog.require('gn_search_default');
 
   var module = angular.module('gn_search_custom',
-       ['gn_search_default']);
+       ['gn_search', 'gn_search_default']);
 
 })();
