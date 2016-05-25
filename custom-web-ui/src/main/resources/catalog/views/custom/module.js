49d7f986ea9e5653ea06af9928171e0983bd43fb
Oscar Fonts <oscar.fonts@gmail.com>
2016-05-25 10:51:31 +0200

    Primera parte del taller

---
+++ custom-web-ui/src/main/resources/catalog/views/custom/module.js
@@ -0,0 +1,10 @@
+(function() {
+
+  goog.provide('gn_search_custom');
+
+  goog.require('gn_search_default');
+
+  var module = angular.module('gn_search_custom',
+       ['gn_search_default']);
+
+})();
