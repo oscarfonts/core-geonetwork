925e25c46afdb00cc4472929efe65b259eb2f9d4
Oscar Fonts <oscar.fonts@gmail.com>
2016-06-29 17:06:10 +0200

    Added custom ICGC background layers to map views

--- web-ui/src/main/resources/catalog/views/custom/config.js
+++ web-ui/src/main/resources/catalog/views/custom/config.js
@@ -57,29 +57,131 @@
 
           };
 
+
+          /*******************************************************************
+             * Define IDEC Base Layers
+             */
+          var URL = {
+              MON: 'http://www.{a-c}.instamaps.cat/mapcache/tms/1.0.0/mon3857@GM8/{z}/{x}/{-y}.jpeg',
+              MQ: 'http://otile{1-4}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png',
+              TOPOICC: 'http://mapcache.{a-c}.icc.cat/map/bases_noutm/wmts/topo/GRID3857/{z}/{x}/{y}.jpeg',
+              TOPOICC_GEO_MON: 'http://www.{a-c}.instamaps.cat/mapcache/tms/1.0.0/A250MON@GM14/{z}/{x}/{-y}.png',
+              TOPOICC_GEO_1: 'http://www.{a-c}.instamaps.cat/mapcache/tms/1.0.0/A250TARJ3857@GMTOT/{z}/{x}/{-y}.png',
+              ESRI: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+              ORTOINSTAMAPS: 'http://www.{a-c}.instamaps.cat/mapcache/tms/1.0.0/orto3857_12@GMTOT/{z}/{x}/{-y}.png',
+              ORTOICC: 'http://mapcache.{a-c}.icc.cat/map/bases_noutm/wmts/orto/GRID3857/{z}/{x}/{y}.jpeg',
+              HIBRIDICGC: 'http://www.{a-c}.instamaps.cat/mapcache/tms/1.0.0/hibrid3857@GMTOT/{z}/{x}/{-y}.png'
+          }
+
+          function resolution(zoom) {
+            var equator = 40075016.686;
+            var tilesize = 256;
+            return equator / (tilesize*Math.pow(2,zoom));
+          };
+
+          function layer(options) {
+            return new ol.layer.Tile({
+              minResolution: resolution(options.maxZoom)-1,
+              maxResolution: resolution(options.minZoom)+1,
+              source: new ol.source.XYZ({
+                url: options.url,
+                minZoom: options.minZoom,
+                maxZoom: options.maxZoom
+              })
+            });
+          }
+
+          var backgrounds = {
+            topo: [
+              layer({
+                url: URL.MON,
+                minZoom: 0,
+                maxZoom: 6
+              }),
+              layer({
+                url: URL.MQ,
+                minZoom: 7,
+                maxZoom: 19
+              }),
+              layer({
+                url: URL.TOPOICC,
+                minZoom: 7,
+                maxZoom: 20
+              })
+            ],
+            simple: [
+              layer({
+                url: URL.TOPOICC_GEO_MON,
+                minZoom: 1,
+                maxZoom: 14
+              }),
+              layer({
+                url: URL.MQ,
+                minZoom: 15,
+                maxZoom: 18
+              }),
+              layer({
+                url: URL.TOPOICC_GEO_1,
+                minZoom: 8,
+                maxZoom: 18
+              })
+            ],
+            orto: [
+              layer({
+                url: URL.ESRI,
+                minZoom: 0,
+                maxZoom: 19
+              }),
+              layer({
+                url: URL.ORTOINSTAMAPS,
+                minZoom: 0,
+                maxZoom: 12
+              }),
+              layer({
+                url: URL.ORTOICC,
+                minZoom: 13,
+                maxZoom: 20
+              })
+            ],
+            hibrid: [
+              layer({
+                url: URL.ESRI,
+                minZoom: 0,
+                maxZoom: 18
+              }),
+              layer({
+                url: URL.ORTOICC,
+                minZoom: 13,
+                maxZoom: 18
+              }),
+              layer({
+                url: URL.HIBRIDICGC,
+                minZoom: 0,
+                maxZoom: 17
+              })
+            ]
+          };
+
           /*******************************************************************
              * Define maps
              */
           var mapsConfig = {
-            center: [280274.03240585705, 6053178.654789996],
-            zoom: 2
+            center: ol.proj.transform([2.1685, 41.3818], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 8,
+            minZoom: 1
             //maxResolution: 9783.93962050256
           };
 
           var viewerMap = new ol.Map({
             controls: [],
+            layers: backgrounds.simple,
             view: new ol.View(mapsConfig)
           });
 
           var searchMap = new ol.Map({
             controls:[],
-            layers: [new ol.layer.Tile({
-              source: new ol.source.OSM()
-            })],
-            view: new ol.View({
-              center: mapsConfig.center,
-              zoom: 2
-            })
+            layers: backgrounds.simple,
+            view: new ol.View(mapsConfig)
           });
 
 
