d6e84cd62425f6b0061a5b7942ec54b1cb4bc543
Oscar Fonts <oscar.fonts@gmail.com>
2016-07-01 11:26:16 +0200

    Use Open Sans fonts

--- web-ui/src/main/resources/catalog/views/custom/config.js
+++ web-ui/src/main/resources/catalog/views/custom/config.js
@@ -57,6 +57,17 @@
 
           };
 
+          WebFontConfig = {
+            google: { families: [ 'Open+Sans:400,600,700,300:latin' ] }
+          };
+          (function() {
+            var wf = document.createElement('script');
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+          })();
 
           /*******************************************************************
              * Define IDEC Base Layers
